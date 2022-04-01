cf7d01925691d3ff5db22ecc9ab180f841b35ed6
mansi131 <74901152+mansi131@users.noreply.github.com>
2022-04-02 01:57:30 +0530

    Update tests.py

--- desisstockmarket/stocks/tests.py
+++ desisstockmarket/stocks/tests.py
@@ -1,3 +1,40 @@
+from inspect import ClassFoundException
 from django.test import TestCase
 
-# Create your tests here.
+from models import Stock, StockPriceHistory
+
+
+class StockModelsTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Stock.objects.create(stockId="AMZN", stockName="AMAZON INC.",
+                             fullCompanyName="AMAZON INCORPORATION PRIVATE LIMITED, USA",
+                             currentSharePrice=2534.26,
+                             sharesOutstanding=2000,
+                             lastTradedAt='2006-10-25 14:30:59')
+
+    def test_string_method(self):
+        stocks = Stock.objects.get(id=1)
+        expected_string = str(self.stockName)
+        self.assertEqual(str(stocks), expected_string)
+
+
+class StockPriceHistoryModelsTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Setting up Stock object for ForeignKey
+        stocks = Stock.objects.create(stockId="MSFT", stockName="MICROSOFT INC.",
+                                      fullCompanyName="MICROSOFT PRIVATE LIMITED, USA",
+                                      currentSharePrice=235.26,
+                                      sharesOutstanding=65000,
+                                      lastTradedAt='2006-10-25 12:30:59')
+
+        StockPriceHistory.objects.create(stock=stocks,
+                                         stockPrice=232.85,
+                                         updatedAt='2006-10-25 12:32:59')
+
+    def test_string_method(self):
+        stockPriceHistoryObject = StockPriceHistory.objects.get(id=1)
+        expected_string = str(self.stock.stockName) + " - " + \
+            str(self.stockPrice) + " - " + str(self.updatedAt)
+        self.assertEqual(str(stockPriceHistoryObject), expected_string)
